46bfa79801e4f5a9444dc1600d5b3a7fdc366656
amar.saraswat <amar.saraswat@senecaglobal.com>
2022-12-12 16:45:50 +0530

    Sign in screen is implemented

---
+++ openapidemo/app/src/main/java/com/intelligent/openapidemo/SignInViewModel.kt
@@ -0,0 +1,44 @@
+package com.intelligent.openapidemo
+
+import android.content.Context
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.sca.in_telligent.openapi.OpenAPI
+import com.sca.seneca.lib.PrintLog
+
+class SignInViewModel:ViewModel () {
+    var loginStatus = MutableLiveData<Boolean>()
+
+
+    fun authorizeDevice(context: Context) {
+        val pushTokenSentToServer = TestApplication.pushToken
+        PrintLog.print("Tag", "***********authorizeDevice() ${TestApplication.pushToken}******")
+        OpenAPI.authorization(pushTokenSentToServer) { status ->
+            if (status.isSuccess) {
+                PrintLog.print("Tag", "Verifying login status - Logged in ")
+                SharedPreferencesHelper.setLogedIn(context, status.isSuccess)
+               loginStatus.value = status.isSuccess
+                PrintLog.print("Tag", "********Login success. Push token sent to server is $pushTokenSentToServer")
+                if (pushTokenSentToServer == "NA" && TestApplication.pushToken != "NA") {
+                    PrintLog.print("Tag", "********Login success. Push token is $pushTokenSentToServer. Hence we call the registerPush API")
+                    registerPush(context, TestApplication.pushToken)
+                } else {
+                    PrintLog.print("Tag", "********Login success. Push token sent to server is ${TestApplication.pushToken} As push token is sent to server, storing in SPF")
+                    SharedPreferencesHelper.setFcmToken(context, TestApplication.pushToken)
+                }
+            } else {
+                loginStatus.value = false
+            }
+        }
+    }
+    private fun registerPush(context:Context, token: String) {
+        OpenAPI.registerPushToken(
+            token
+        ) {
+
+            SharedPreferencesHelper.setFcmToken(context, TestApplication.pushToken)
+
+        }
+    }
+
+}
